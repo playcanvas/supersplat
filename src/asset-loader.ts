c3f92db78fd202bbd11cb747b90596ff52bbbf1c
Donovan Hutchence <slimbuck7@gmail.com>
2024-07-08 14:34:11 +0100

    update deps, camera control, simple 2d gs support

--- src/asset-loader.ts
+++ src/asset-loader.ts
@@ -53,6 +53,18 @@ class AssetLoader {
                 );
                 asset.on('load', () => {
                     stopSpinner();
+
+                    // support loading 2d splats by adding scale_2 property with almost 0 scale
+                    const splatData = asset.resource.splatData;
+                    if (splatData.getProp('scale_0') && splatData.getProp('scale_1') && !splatData.getProp('scale_2')) {
+                        const scale2 = new Float32Array(splatData.numSplats);
+                        const value = Math.log(1e-6);
+                        for (let i = 0; i < splatData.numSplats; i++) {
+                            scale2[i] = value;
+                        }
+                        splatData.addProp('scale_2', scale2);
+                    }
+
                     resolve(new Splat(asset));
                 });
                 asset.on('error', (err: string) => {

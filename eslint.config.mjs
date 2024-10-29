06d6c479f0cde12bccbd32675493c890521ceceb
Donovan Hutchence <slimbuck7@gmail.com>
2024-10-29 15:22:32 +0000

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,5 +34,6 @@ export default [{
         "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-unsafe-function-type": "off",
     },
 }];

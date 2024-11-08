79a1fe20456a5bd2ffd82ac4ac6c0f2875834d26
Donovan Hutchence <slimbuck7@gmail.com>
2024-11-08 15:37:58 +0000

    Update sphere shader to work like infinite grid (#275)

--- src/shaders/sphere-shape-shader.ts
+++ src/shaders/sphere-shape-shader.ts
@@ -34,11 +34,6 @@ const fragmentShader = /* glsl */ `
         return (v.z / v.w) * 0.5 + 0.5;
     }
 
-    float noise(vec2 fragCoord, sampler2D noiseTex) {
-        vec2 uv = fract(fragCoord / 32.0);
-        return texture2DLodEXT(noiseTex, uv, 0.0).y;
-    }
-
     vec2 calcAzimuthElev(in vec3 dir) {
         float azimuth = atan(dir.z, dir.x);
         float elev = asin(dir.y);
@@ -48,39 +43,6 @@ const fragmentShader = /* glsl */ `
     uniform sampler2D blueNoiseTex32;
     uniform mat4 matrix_viewProjection;
     uniform vec4 sphere;
-
-    bool strips(vec3 lp) {
-        vec2 ae = calcAzimuthElev(normalize(lp));
-
-        float spacing = 180.0 / (2.0 * 3.14159 * sphere.w);
-        float size = 0.03;
-        return fract(ae.x / spacing) > size &&
-               fract(ae.y / spacing) > size;
-    }
-
-    void behind(vec3 rayOrigin, vec3 rayDirection, float t) {
-        vec3 wp = rayOrigin + rayDirection * t;
-        if (strips(wp - sphere.xyz) || noise(gl_FragCoord.yx, blueNoiseTex32) < 0.125) {
-            discard;
-        }
-
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_FragDepth = calcDepth(wp, matrix_viewProjection);
-    }
-
-    void front(vec3 rayOrigin, vec3 rayDirection, float t0, float t1) {
-        if (t0 < 0.0) {
-            behind(rayOrigin, rayDirection, t1);
-        } else {
-            vec3 wp = rayOrigin + rayDirection * t0;
-            if (strips(wp - sphere.xyz) || noise(gl_FragCoord.xy, blueNoiseTex32) < 0.6) {
-                behind(rayOrigin, rayDirection, t1);
-            } else {
-                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-                gl_FragDepth = calcDepth(wp, matrix_viewProjection);
-            }
-        }
-    }
 
     uniform vec3 near_origin;
     uniform vec3 near_x;
@@ -92,19 +54,48 @@ const fragmentShader = /* glsl */ `
 
     uniform vec2 targetSize;
 
+    bool writeDepth(float alpha) {
+        vec2 uv = fract(gl_FragCoord.xy / 32.0);
+        float noise = texture2DLodEXT(blueNoiseTex32, uv, 0.0).y;
+        return alpha > noise;
+    }
+
+    bool strips(vec3 lp) {
+        vec2 ae = calcAzimuthElev(normalize(lp));
+
+        float spacing = 180.0 / (2.0 * 3.14159 * sphere.w);
+        float size = 0.03;
+        return fract(ae.x / spacing) < size ||
+               fract(ae.y / spacing) < size;
+    }
+
     void main() {
         vec2 clip = gl_FragCoord.xy / targetSize;
         vec3 worldNear = near_origin + near_x * clip.x + near_y * clip.y;
         vec3 worldFar = far_origin + far_x * clip.x + far_y * clip.y;
 
-        vec3 ray = normalize(worldFar - worldNear);
+        vec3 rayDir = normalize(worldFar - worldNear);
 
         float t0, t1;
-        if (!intersectSphere(t0, t1, worldNear, ray, sphere)) {
+        if (!intersectSphere(t0, t1, worldNear, rayDir, sphere)) {
             discard;
         }
 
-        front(worldNear, ray, t0, t1);
+        vec3 frontPos = worldNear + rayDir * t0;
+        bool front = t0 > 0.0 && strips(frontPos - sphere.xyz);
+
+        vec3 backPos = worldNear + rayDir * t1;
+        bool back = strips(backPos - sphere.xyz);
+
+        if (front) {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 0.6);
+            gl_FragDepth = writeDepth(0.6) ? calcDepth(frontPos, matrix_viewProjection) : 1.0;
+        } else if (back) {
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 0.6);
+            gl_FragDepth = writeDepth(0.6) ? calcDepth(backPos, matrix_viewProjection) : 1.0;
+        } else {
+            discard;
+        }
     }
 `;
 

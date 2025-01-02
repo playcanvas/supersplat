d1cab3e1781538da8e1af6d820b446812f5c7d4d
Donovan Hutchence <slimbuck7@gmail.com>
2025-01-02 09:57:55 +0000

    Fix undo/redo keyboard shortcut (#361)

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -4,6 +4,7 @@ interface ShortcutOptions {
     ctrl?: boolean;
     shift?: boolean;
     sticky?: boolean;
+    capture?: boolean;      // use capture phase - i.e. handle the events before anyone else
     func?: () => void;
     event?: string;
 }
@@ -14,17 +15,21 @@ class Shortcuts {
     constructor(events: Events) {
         const shortcuts = this.shortcuts;
 
-        const handleEvent = (e: KeyboardEvent, down: boolean) => {
+        const handleEvent = (e: KeyboardEvent, down: boolean, capture: boolean) => {
             // skip keys in input fields
-            if (e.target !== document.body) return;
+            if (!capture && e.target !== document.body) return;
 
             for (let i = 0; i < shortcuts.length; i++) {
                 const shortcut  = shortcuts[i];
                 const options = shortcut.options;
 
                 if (shortcut.keys.includes(e.key) &&
+                    ((options.capture ?? false) === capture) &&
                     !!options.ctrl === !!(e.ctrlKey || e.metaKey) &&
                     !!options.shift === !!e.shiftKey) {
+
+                    e.stopPropagation();
+                    e.preventDefault();
 
                     // handle sticky shortcuts
                     if (options.sticky) {
@@ -53,12 +58,21 @@ class Shortcuts {
 
         // register keyboard handler
         document.addEventListener('keydown', (e) => {
-            handleEvent(e, true);
+            handleEvent(e, true, false);
         });
 
         document.addEventListener('keyup', (e) => {
-            handleEvent(e, false);
+            handleEvent(e, false, false);
         });
+
+        // also handle capture phase
+        document.addEventListener('keydown', (e) => {
+            handleEvent(e, true, true);
+        }, true);
+
+        document.addEventListener('keyup', (e) => {
+            handleEvent(e, false, true);
+        }, true);
     }
 
     register(keys: string[], options: ShortcutOptions) {

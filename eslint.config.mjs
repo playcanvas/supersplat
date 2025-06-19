d7c5252952cc14a908dd7209d29e99cb2cdbc040
kpal <kpal81xd@gmail.com>
2025-06-19 12:09:59 +0100

    Fixed lint in rollup config mjs file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
 import playcanvasConfig from '@playcanvas/eslint-config';
-import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
 
 export default [
@@ -23,17 +23,27 @@ export default [
             }
         },
         rules: {
-            ...tsPlugin.configs['recommended'].rules,
+            ...tsPlugin.configs.recommended.rules,
             '@typescript-eslint/ban-ts-comment': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
             'lines-between-class-members': 'off',
             'no-await-in-loop': 'off',
-            'require-atomic-updates': 'off',
+            'require-atomic-updates': 'off'
+        }
+    }, {
+        files: ['**/*.mjs'],
+        languageOptions: {
+            globals: {
+                ...globals.node
+            }
+        },
+        rules: {
+            'import/no-unresolved': 'off'
         }
     }, {
         ignores: [
             'submodules/'
-        ],
+        ]
     }
 ];

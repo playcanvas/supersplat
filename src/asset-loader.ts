c8e3d53e85d0ccf88607e001d59932ae4c62ab79
Donovan Hutchence <slimbuck7@gmail.com>
2025-10-16 16:40:53 +0100

    Fix lcc default orientation (#613)

--- src/asset-loader.ts
+++ src/asset-loader.ts
@@ -1,4 +1,4 @@
-import { AppBase, Asset, GSplatData, GSplatResource } from 'playcanvas';
+import { AppBase, Asset, GSplatData, GSplatResource, Vec3 } from 'playcanvas';
 
 import { Events } from './events';
 import { loadLcc } from './loaders/lcc';
@@ -6,6 +6,9 @@ import { ModelLoadRequest } from './loaders/model-load-request';
 import { loadPly } from './loaders/ply';
 import { loadSplat } from './loaders/splat';
 import { Splat } from './splat';
+
+const defaultOrientation = new Vec3(0, 0, 180);
+const lccOrientation = new Vec3(90, 0, 180);
 
 // handles loading gltf container assets
 class AssetLoader {
@@ -39,16 +42,18 @@ class AssetLoader {
             const filename = (loadRequest.filename || loadRequest.url).toLowerCase();
 
             let asset;
+            let orientation = defaultOrientation;
 
             if (filename.endsWith('.splat')) {
                 asset = wrap(await loadSplat(loadRequest));
             } else if (filename.endsWith('.lcc')) {
                 asset = wrap(await loadLcc(loadRequest));
+                orientation = lccOrientation;
             } else {
                 asset = await loadPly(this.app.assets, loadRequest);
             }
 
-            return new Splat(asset);
+            return new Splat(asset, orientation);
         } finally {
             if (!loadRequest.animationFrame) {
                 this.events.fire('stopSpinner');

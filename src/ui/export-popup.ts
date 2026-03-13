6ef99bcbdfee52328524ae06017bfdbf498cefc7
Donovan Hutchence <slimbuck7@gmail.com>
2026-03-13 11:08:07 +0000

    latest

--- src/ui/export-popup.ts
+++ src/ui/export-popup.ts
@@ -432,9 +432,7 @@ class ExportPopup extends Container {
                 const pose = events.invoke('camera.getPose');
                 const p = pose?.position;
                 const t = pose?.target;
-                const hasStartPose = !!(p && t);
-
-                const cameras = hasStartPose ? [{
+                const cameras = (p && t) ? [{
                     initial: {
                         position: [p.x, p.y, p.z] as [number, number, number],
                         target: [t.x, t.y, t.z] as [number, number, number],
@@ -483,8 +481,7 @@ class ExportPopup extends Container {
                     animTracks,
                     cameras,
                     annotations: [],
-                    startMode: includeAnimation ? 'animTrack' : 'default',
-                    hasStartPose
+                    startMode: includeAnimation ? 'animTrack' : 'default'
                 };
 
                 return {
